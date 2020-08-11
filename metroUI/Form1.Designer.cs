30c22c8b1b3097687ebff890d1e3b6547f01b563
unknown <jean-pierre14>
2020-08-11 09:38:48 +0200

    Meteo Design C# codes

---
+++ metroUI/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace metroUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlBttn = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PannelAcceuil = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pnlBttn);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(103, 444);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlBttn
+            // 
+            this.pnlBttn.BackColor = System.Drawing.Color.Yellow;
+            this.pnlBttn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBttn.Location = new System.Drawing.Point(5, 53);
+            this.pnlBttn.Name = "pnlBttn";
+            this.pnlBttn.Size = new System.Drawing.Size(12, 23);
+            this.pnlBttn.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PannelAcceuil
+            // 
+            this.PannelAcceuil.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PannelAcceuil.Location = new System.Drawing.Point(103, 0);
+            this.PannelAcceuil.Name = "PannelAcceuil";
+            this.PannelAcceuil.Size = new System.Drawing.Size(509, 444);
+            this.PannelAcceuil.TabIndex = 1;
+            this.PannelAcceuil.Paint += new System.Windows.Forms.PaintEventHandler(this.panelacc_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 444);
+            this.Controls.Add(this.PannelAcceuil);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PannelAcceuil;
+        private System.Windows.Forms.Panel pnlBttn;
+    }
+}
+
---
+++ metroUI/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metroUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pnlBttn.Height = button1.Height;
+            pnlBttn.Top = button1.Top;
+            PannelAcceuil.BringToFront();
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pnlBttn.Height = button1.Height;
+            pnlBttn.Top = button1.Top;
+            PannelAcceuil.BringToFront();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pnlBttn.Height = button2.Height;
+            pnlBttn.Top = button2.Top;
+          //  PannelDash.BringToFront();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pnlBttn.Height = button3.Height;
+            pnlBttn.Top = button3.Top;
+           // PannelClose.BringToFront();
+
+        }
+
+        private void panelvist_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void panelacc_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+              pnlBttn.Height = button1.Height;
+             pnlBttn.Top = button1.Top;
+            PannelAcceuil.BringToFront();
+            pnlBttn.Hide();
+        }
+    }
+}
